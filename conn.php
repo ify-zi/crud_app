a3700cbd515dbbafc469e43f2a47ca196e6efaa9
Ifeanyi Zion Chidiebere <64507351+ify-zi@users.noreply.github.com>
2021-03-19 22:51:09 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -13,12 +13,12 @@
 // connect to db
     $conn = mysqli_connect($db_server, $db_username, $db_password, $db);
     
-         $sql = "CREATE TABLE info ( ".
-            "id INT NOT NULL AUTO_INCREMENT, ".
-            "s_name VARCHAR(100) NOT NULL, ".
-            "stock VARCHAR(100) NOT NULL, ".
-            "amount INT(100) NOT NULL, ".
-            "PRIMARY KEY (id)); ";
+         $sql = "CREATE TABLE info ( 
+            id INT(30) NOT NULL AUTO_INCREMENT, 
+            s_name VARCHAR(100) NOT NULL, 
+            stock VARCHAR(100) NOT NULL, 
+            amount INT(100) NOT NULL, 
+            PRIMARY KEY (id)) ";
          //mysql_select_db($db);
          mysqli_query( $sql, $conn );
          
