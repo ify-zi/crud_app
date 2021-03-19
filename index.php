dac97684da02f8b4d37f8543e5dde27f345f5927
Ifeanyi Zion Chidiebere <64507351+ify-zi@users.noreply.github.com>
2021-03-19 22:32:35 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -38,7 +38,9 @@
             <td> <?php echo $row['stock']; ?></td>
             <td> <?php echo $row['amount']; ?></td>
 
-            <td> <a href="update.php?edit=<?php echo $row['id']; ?>" class=" btn btn-primary">EDIT</a></td>
+            <td> <form action="update.php" method="GET" >
+            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                <button type="submit" name="edit" value= class=" btn btn-primary">EDIT</button></form></td>
             <td> <a href="php_code.php?del=<?php echo $row['id']; ?>" class="btn btn-danger">DELETE</a></td>
         </tr>
         <?php  } }else{ echo "<p class='lead'><em> No Records were Found.</em></p>";}}else {echo "ERROR: Could not able to execute . " . mysqli_error($conn);
@@ -92,7 +94,7 @@
 
  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-   <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
  -->
